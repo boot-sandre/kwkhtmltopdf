b1ae31b9223512db277e9a556d44c63fecc09154
Simon ANDRE <simon@emencia.com>
2024-11-22 14:09:53 +0100

    Add to client request google auth token fetch from cloud run credentials

--- client/python/kwkhtmltopdf_client.py
+++ client/python/kwkhtmltopdf_client.py
@@ -7,9 +7,36 @@ from __future__ import print_function
 import os
 import sys
 
+import googleapiclient.discovery
+import google.auth
+import google.auth.exceptions
+
+_, PROJECT_ID = google.auth.default()
+
 import requests
 
 CHUNK_SIZE = 2 ** 16
+
+
+def get_service_url():
+    try:
+        service = googleapiclient.discovery.build('run', 'v1')
+    except google.auth.exceptions.DefaultCredentialsError as Exc:
+        raise Error(Exc)
+    else:
+        k_service = os.environ['K_SERVICE']
+        parent = f'namespaces/{PROJECT_ID}'
+
+        request = service.namespaces().services().list(parent=parent)
+        response = request.execute()
+
+        serv_url = ""
+        for item in response['items']:
+            if item['metadata']['name'] == k_service:
+                serv_url = item['status']['url']
+                break
+
+    return serv_url
 
 
 class Error(Exception):
@@ -65,7 +92,15 @@ def wkhtmltopdf(args):
         add_option("-h")
 
     try:
-        r = requests.post(url, files=parts)
+        audience = get_service_url()
+        endpoint = url
+        req = urllib.request.Request(endpoint)
+        auth_req = google.auth.transport.requests.Request()
+        id_token = google.oauth2.id_token.fetch_id_token(auth_req, audience)
+        headers = {
+            "Authorization": f"Bearer {id_token}",
+        }
+        r = requests.post(url, files=parts, headers=headers)
         r.raise_for_status()
 
         if output == "-":
